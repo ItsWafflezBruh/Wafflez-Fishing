ae528017212fd2e5830f4402b2a862ef58b90e68
ItsWafflezBruh <deanbean1305@gmail.com>
2025-05-19 03:24:15 -0500

    Update import.sql
    Error in syntax

--- Wafflez-Fishing/import.sql
+++ Wafflez-Fishing/import.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXIST `wafflez_fishing` (
+CREATE TABLE IF NOT EXISTS `wafflez_fishing` (
     `citizen_id` varchar(255) NOT NULL,
     `rep` float NOT NULL DEFAULT 0,
     `fish_caught` int NOT NULL DEFAULT 0,
